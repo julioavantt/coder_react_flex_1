b74ab3c1c8d827f457838012e8b96fd8d78e2643
pepe <pepe@pepe.com>
2025-01-27 02:03:50 -0300

    first commit

---
+++ repaso_js/expresiones.js
@@ -0,0 +1,30 @@
+console.log(2 + 3);
+
+console.log([]);
+
+console.log([] + 1);
+
+console.log("hola"[1]);
+
+console.log(1 + "");
+console.log("" + 1);
+console.log(typeof (1 + ""));
+console.log(typeof ("" + 1));
+console.log(typeof ("" - 1));
+
+console.log(1 == true);
+console.log(NaN == NaN);
+console.log([] == []);
+
+console.log([[[]]].length);
+
+console.log(1 === "1");
+
+console.log("1" === 1);
+
+const obj1 = {};
+
+const obj2 = obj1;
+
+console.log(obj1 === obj2);
+console.log({} == {});
---
+++ repaso_js/funciones.js
@@ -0,0 +1,5 @@
+const fn = function () {
+ return 1;
+};
+
+console.log(fn());
